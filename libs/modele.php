55c90460960523707c9981220c48772109a1d3a5
huguette642 <75128021+huguette642@users.noreply.github.com>
2023-05-23 09:45:43 +0200

    Add files via upload

--- libs/modele.php
+++ libs/modele.php
@@ -39,9 +39,62 @@ function obtenirClient() {
 	return parcoursRs(SQLSelect($SQL));
 }
 
+function obtenirBilanTotal() {
+	$var1 = obtenirBeneficeTotal();
+	$var2 = obtenirNbVente();
+	$var3 = obtenirNbStock();
+	return $var1 . $var2 . $var3;
+}
+function obtenirBeneficeTotal() {
+	$SQL = "SELECT (SUM(v.Resale_price)-SUM(s.Retail_price))
+			FROM Stock AS s
+			JOIN Sales AS v
+			ON s.Idshoes = v.Idshoes";
+	return SQLGetChamp($SQL);
+}
+
+function obtenirNbVente() {
+	$SQL = "SELECT COUNT(*)
+			FROM Sales";
+	return SQLGetChamp($SQL);
+}
+
+function obtenirNbStock() {
+	$SQL = "SELECT COUNT(*)
+			FROM Stock";
+	return SQLGetChamp($SQL);
+}
+
+function obtenirBilanAnnee($date) {
+	$var1 = obtenirBeneficeAnnee($date);
+	$var2 = obtenirNbVenteAnnee($date);
+	$var3 = obtenirNbStockAnnee($date);
+	return $var1 . $var2 . $var3;
+}
 
 
+function obtenirBeneficeAnnee($date) {
+	$SQL = "SELECT (SUM(v.Resale_price)-SUM(s.Retail_price))
+			FROM Stock AS s
+			JOIN Sales AS v
+			ON s.Idshoes = v.Idshoes
+			WHERE s.Resale_date>'$date' AND s.Retail_date>'$date'";
+	return SQLGetChamp($SQL);
+}
 
+function obtenirNbVenteAnnee($date) {
+	$SQL = "SELECT COUNT(*)
+			FROM Sales
+			WHERE Resale_date>'$date'";
+	return SQLGetChamp($SQL);
+}
+
+function obtenirNbStockAnnee($date) {
+	$SQL = "SELECT COUNT(*)
+			FROM Stock
+			WHERE Retail_date>'$date'";
+	return SQLGetChamp($SQL);
+}
 
 
 ?>
